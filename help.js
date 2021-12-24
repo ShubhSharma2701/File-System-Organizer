9e32909f37e37a3e34bac29d9f35608a414aaa95
Shubham Sharma <95160816+ShubhSharma2701@users.noreply.github.com>
2021-12-24 13:46:28 -0500

    Add files via upload

---
+++ help.js
@@ -0,0 +1,7 @@
+// help implemented
+function helpfn(dirpath){
+    console.log('List of all commands: node input.js tree "directorypath" node input.js organize "directory path" node input.js help ');
+}
+module.exports = {
+    helpKey: helpfn
+}
