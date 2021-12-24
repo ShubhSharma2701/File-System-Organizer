1371f954fd76127e10b6838ca4a4f99a93d316a4
Shubham Sharma <95160816+ShubhSharma2701@users.noreply.github.com>
2021-12-24 14:02:24 -0500

    Update organize.js

--- organize.js
+++ organize.js
@@ -8,54 +8,54 @@ function organizefn(dirpath){
     }else{
         let doesExist= fs.existsSync(dirpath);
         if (doesExist){
-            //2. create- organized files naam ki dir bnaunga jisme saari files organised pdi hongi
-             destPath= path.join(dirpath,"organized_files"); //organised_files naam ki dir bnadega
-             if(fs.existsSync(destPath)==false){ //condition lgai h ki tbhi bnaega agar present ni hogi pehle se
+            //2. create- "organized_files" named dir where all files will be categorized
+             destPath= path.join(dirpath,"organized_files"); //organised_files named dir created 
+             if(fs.existsSync(destPath)==false){ //if dir not present with same name only then create 
                  fs.mkdirSync(destPath);
              }
             
         }else{
-            console.log("kindly enter correct path");return; //pehle se present hui to ye line print krdega
+            console.log("kindly enter correct path");return; 
         }
     }
-    organizeHelper(dirpath,destPath) //yahn fn define krdia h bnaya neeche h alag se yahan bheed jyada hogyi thi   
+    organizeHelper(dirpath,destPath) 
     
 } 
-function organizeHelper(src,dest){  // ye source se destination k beech ki saari files identify krdega
-    //3. identify- konsi category ki file h- docx,pdf,etc.
+function organizeHelper(src,dest){  // fn for identifying diff categories 
+
     let childnames= fs.readdirSync(src); 
-   //console.log(childnames); //saari files jo projectFolder wali dir m pdi hain unka naam dedega
+   //console.log(childnames); 
     for(let i=0; i<childnames.length;i++){
-        let childAddress= path.join(src,childnames[i]); //ye path join krne k lie h
-      let isFile= fs.lstatSync(childAddress).isFile(); // agar file hi h sirf tbhi kaam krega
-        if(isFile){ //ye sirf files k naam dega koi folder hua to uska chor dega
+        let childAddress= path.join(src,childnames[i]); //for joining dir and file path
+      let isFile= fs.lstatSync(childAddress).isFile(); // will only work if there is a file and not folder
+        if(isFile){ 
         //console.log(childnames[i]);
           let category= getCategory(childnames[i]);
           console.log("Given", childnames[i],"is of", category,"type")
         sendFiles(childAddress,dest,category);
-        }//4 copy/cut- organised dir m sbko daaldio,is dir m organised folders honge media,doc etc. 
+        }
     }
 }
      
-function getCategory(name){ // ye fn btadega ki konse category ki file h, pdf,docx,etc
+function getCategory(name){ // fn for categorising files 
 let ext=path.extname(name);
-ext= ext.slice(1); //2nd idx se islie lenge kyuki 1st idx p . pda hua h, fir aisa dikhega .js,.pdf, .docx
+ext= ext.slice(1); //taken from 2nd idx from bcoz 1st idx contains ., so it will look like docx,.pdf but we want only docx,pdf
 for (let type in types){
-   let filetype= types[type] // poore folder p loop lgakr ab categories bankr unme daaldi files
+   let filetype= types[type] // run a loop on whole folder and copy files in req categories 
    for (let i=0; i<filetype.length;i++){
     if (ext==filetype[i]){
         return type;
     }
    }    
 }
- return "others"; //agar koi category ni h given type array m se to ek  others folder bnakr vo sb usme daaldia
+ return "others"; //if  a file from no category found from given array then create dir named others and put the file in it
 }
-function sendFiles(srcFilePath,dest,category){ //file copy krne k lie fn 
+function sendFiles(srcFilePath,dest,category){ //fn for copying files
 let categoryPath= path.join(dest,category);
-if(fs.existsSync(categoryPath)==false){ //agar same naam ki file exist ni krti to bnade
+if(fs.existsSync(categoryPath)==false){ 
    fs.mkdirSync(categoryPath)
 }
-let fileName= path.basename(srcFilePath); // jb hm file ko copy paste krte hain tb hmara os ek nai blank file bnata h pehle same naam ki fir usme saara content copy krdeta h yahan bhi yhi kia h 
+let fileName= path.basename(srcFilePath);  
 let destFilePath= path.join(categoryPath,fileName);
 fs.copyFileSync(srcFilePath,destFilePath);
 console.log(fileName,"copied to", category)
